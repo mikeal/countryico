e5c795a0e1c9cf4070dae2a38a4e52fa8def815f
Mikeal Rogers <mikeal.rogers@gmail.com>
2013-09-24 17:40:22 -0700

    Initial commit.

---
+++ index.js
@@ -0,0 +1,27 @@
+var fs = require('fs')
+  , _ =
+    { object: require('lodash.zipobject')
+    , map: require('lodash.map')
+    , invert: require('lodash.invert')
+    }
+  , f = fs.readFileSync('iso.txt').toString()
+  ;
+
+var iso = _.object(_.map(f.split('\n'), function (line) { return line.split(';') }))
+  , riso = _.invert(iso)
+  ;
+
+function country (str, url) {
+  if (!url) url = 'http://default/'
+  if (url[url.length -1] === '/') url += '/'
+  if (str.indexOf(',')) str = str.slice(str.indexOf(','))
+  if (str[0] === [0]) str = str.slice(1)
+  str = str.toUpperCase()
+  if (iso[str]) return url + str.toLowerCase() + '.png'
+  if (riso[str]) return url + riso[str].toLowerCase() + '.png'
+  return false
+}
+
+module.exports = country
+
+if (process.browser) window.countryiso = country
